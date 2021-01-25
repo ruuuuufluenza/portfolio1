74ac7dced12907c53bcfc7cff029fdd146ee2288
ruuuuufluenza <77866955+ruuuuufluenza@users.noreply.github.com>
2021-01-25 19:25:48 +0900

    initial commit
    練習です

---
+++ index.js
@@ -0,0 +1,38 @@
+// メニューボタンここから
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById("menu-trigger").addEventListener("click", function(){
+        this.classList.toggle("is-active");
+        document.getElementById("nav").classList.toggle("is-active");
+        document.getElementById("mask").classList.toggle("is-active");
+
+    })
+});
+
+// swiperここから
+const mySwiper = new Swiper('.swiper-container',{
+    effect: 'fade',
+    loop: true,
+    speed:5000,
+    autoplay:{
+        delay:5000
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+  });
+
+//   スクロールしたら
+
+const targetElement = document.querySelectorAll(".animationTarget");
+document.addEventListener("scroll", function(){
+
+    for (let i = 0; i < targetElement.length; i ++) {
+        const getElementDistance = targetElement[i].
+        getBoundingClientRect().top + targetElement[i].clientHeight * .4
+        if (window.innerHeight > getElementDistance) {
+            targetElement[i].classList.add("show");
+        }
+    
+    }
+})
